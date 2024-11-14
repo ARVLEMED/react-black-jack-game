c259e7d4cc9f584827d309c1b5d8e84584a67ae4
aron k koech <varokoaruna81@gmail.com>
2024-11-14 10:03:10 +0300

    updated css

--- src/components/Word.js
+++ src/components/Word.js
@@ -42,36 +42,6 @@ const BlackjackTutorial = () => {
     </div>
   );
 
-  const Techniques = () => (
-    <div id="game-techniques">
-      <h2>Game Play Techniques</h2>
-      <h3>Double Down</h3>
-      <p>
-        This is an option open to the player for doubling their bet when the
-        original two cards dealt total 9, 10, or 11. When the player's turn
-        comes, they place a bet equal to the original bet, and the dealer gives
-        the player just one card, which is placed face down and is not turned up
-        until the bets are settled at the end of the hand.
-      </p>
-      <h3>Surrendering</h3>
-      <p>
-        If you have an especially bad hand, you can "surrender" to the dealer
-        instead of hitting or staying. Surrendering deducts 50% off your
-        original bet, but it can save you from a total loss. Note that not all
-        casinos may allow surrendering.
-      </p>
-      <h3>Insurance</h3>
-      <p>
-        When the dealer's face-up card is an ace, players may make a side bet of
-        up to half the original bet that the dealer's face-down card is a
-        ten-card, and thus a blackjack for the house. If it is, those players
-        win the insurance bet with a 2 to 1 payoff. Insurance is generally not a
-        good proposition for the player unless they are confident that there are
-        an unusually high number of ten-cards still left undealt.
-      </p>
-    </div>
-  );
-
   const Tips = () => (
     <div id="game-tips">
       <h2>How to Play</h2>
@@ -83,6 +53,9 @@ const BlackjackTutorial = () => {
         <li>
           Receive your cards: Once all bets have been placed, the dealer will
           deal two cards to each player, face up.
+          <div>
+        <img src={require("./screenshot.png")}></img>
+          </div>
         </li>
         <li>
           Decide to hit or stand: After receiving your two cards, you can choose
@@ -153,6 +126,36 @@ const BlackjackTutorial = () => {
     </div>
   );
 
+  const Techniques = () => (
+    <div id="game-techniques">
+      <h2>Game Play Techniques</h2>
+      <h3>Double Down</h3>
+      <p>
+        This is an option open to the player for doubling their bet when the
+        original two cards dealt total 9, 10, or 11. When the player's turn
+        comes, they place a bet equal to the original bet, and the dealer gives
+        the player just one card, which is placed face down and is not turned up
+        until the bets are settled at the end of the hand.
+      </p>
+      <h3>Surrendering</h3>
+      <p>
+        If you have an especially bad hand, you can "surrender" to the dealer
+        instead of hitting or staying. Surrendering deducts 50% off your
+        original bet, but it can save you from a total loss. Note that not all
+        casinos may allow surrendering.
+      </p>
+      <h3>Insurance</h3>
+      <p>
+        When the dealer's face-up card is an ace, players may make a side bet of
+        up to half the original bet that the dealer's face-down card is a
+        ten-card, and thus a blackjack for the house. If it is, those players
+        win the insurance bet with a 2 to 1 payoff. Insurance is generally not a
+        good proposition for the player unless they are confident that there are
+        an unusually high number of ten-cards still left undealt.
+      </p>
+    </div>
+  );
+  
   const Strategy = () => (
     <div id="game-strategy">
       <h2>Strategy</h2>
@@ -196,9 +199,10 @@ const BlackjackTutorial = () => {
     <div className="tutorial-container">
       <Introduction />
       <Rules />
-      <Techniques />
+      
       <Tips />
       <Strategy />
+      <Techniques />
     </div>
   );
 };
